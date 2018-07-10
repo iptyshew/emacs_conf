ad2780d5026aa4e5d14b29170090c70cf89231f0
Dmitria <Dmitria1994@gmail.com>
2018-07-10 23:09:43 +0700

    Поменял список пакетов

--- init.el
+++ init.el
@@ -7,11 +7,6 @@
 (package-initialize)
 
 (add-to-list 'load-path (expand-file-name "settings" user-emacs-directory))
-(require 'custom_pref)
-(require 'plugins)
-(require 'package)
-(require 'my_keybindings)
-
 (add-to-list
    'package-archives
    '("melpa" . "http://melpa.milkbox.net/packages/")
@@ -27,7 +22,7 @@
 	("9b59e147dbbde5e638ea1cde5ec0a358d5f269d27bd2b893a0947c4a867e14c1" "3cd28471e80be3bd2657ca3f03fbb2884ab669662271794360866ab60b6cb6e6" "e9776d12e4ccb722a2a732c6e80423331bcb93f02e089ba2a4b02e85de1cf00e" "72a81c54c97b9e5efcc3ea214382615649ebb539cb4f2fe3a46cd12af72c7607" "58c6711a3b568437bab07a30385d34aacf64156cc5137ea20e799984f4227265" "96998f6f11ef9f551b427b8853d947a7857ea5a578c75aa9c4e7c73fe04d10b4" "987b709680284a5858d5fe7e4e428463a20dfabe0a6f2a6146b3b8c7c529f08b" "306793051a1bd8e3859783c9bbb7335ddbd8349fcaab1c7c2c6aad3d86c99b46" "268ffe7d42e6732185770ff9a2940e11c996dd47843336e5a227a0a06dc432b5" default)))
  '(package-selected-packages
    (quote
-	(flycheck-rtags flycheck-irony company-irony flycheck cmake-ide helm-projectile cmake-mode sr-speedbar powerline magit helm ido-vertical-mode markdown-mode)))
+	(company-irony cmake-ide helm-projectile cmake-mode sr-speedbar powerline magit helm markdown-mode rtags company company-irony projectile irony ##)))
  '(safe-local-variable-values
    (quote
 	((projectile-project-compilation-cmd . "clang++ --std=c++14 -g -O0 test.cpp")))))
@@ -41,3 +36,9 @@
 (put 'narrow-to-region 'disabled nil)
 (put 'narrow-to-page 'disabled nil)
 (put 'set-goal-column 'disabled nil)
+
+
+(require 'custom_pref)
+(require 'plugins)
+(require 'package)
+(require 'my_keybindings)
