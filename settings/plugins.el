0f0ecd2503525110706aab22b7c886dfa32ad677
Dmitriy Iptyshev <diptyshev@ptsecurity.com>
2018-12-17 00:15:40 +0700

    Add dashboard plugin

--- settings/plugins.el
+++ settings/plugins.el
@@ -110,4 +110,13 @@
 (powerline-center-theme)
 
 
+(use-package dashboard
+  :ensure t
+  :config
+  (dashboard-setup-startup-hook)
+  (setq dashboard-items '((recents  . 5)
+                        (projects . 5)
+                        (agenda . 5))))
+
+
 (provide 'plugins)
