263c86beaa86636d76ae260da2f4f8d140453c18
Dmitriy Iptyshev <diptyshev@ptsecurity.com>
2018-12-27 16:58:43 +0700

    wip xp syntax

--- settings/utils.el
+++ settings/utils.el
@@ -2,7 +2,7 @@
 (defun backward-kill-word-or-kill-region (arg)
   (interactive "p")
   (if (region-active-p)
-      (kill-region (region-beginning) 
+      (kill-region (region-beginning)
                    (region-end))
     (backward-kill-word arg)))
 
@@ -30,5 +30,52 @@
                    (forward-word arg)
                    (point)))
       (activate-mark))))
+
+
+
+
+(setq xp-font-lock-keywords
+      (let* (
+            ;; define several category of keywords
+             (x-keywords '("if" "not" "div"
+                           "mod" "and" "or"
+                           "switch" "else"
+                           "elif" "then"
+                           "endif" "summessage"
+                           "subformula" "endsubformula"))
+             (x-types '("CSV" "DATETIME" "DURATION"
+                        "HOSTNAME" "IPV4" "KEYVALUE"
+                        "LITERAL" "MACADDR" "NTUSER"
+                        "NUMBER" "REST" "STRING" "UNTIL"
+                        "WORD" "WORDDASH" "IPV6"))
+             (x-constants '("TEXT" "JSON" "EVENTLOG" "TABULAR" "XML" "COND"))
+             (x-functions '("datetime" "datetime_to_epoch"
+                            "datetime_to_epoch_ms" "datetime_to_win_ticks"
+                            "duration" "epoch_to_datetime" "epoch_ms_to_datetime"
+
+                            "strip" "csv" "find_substr" "keyvalue" "length" "len"
+                            "lower" "match" "substr" "upper"))
+
+             (x-keywords-regexp (regexp-opt x-keywords 'words))
+             (x-types-regexp (regexp-opt x-types 'words))
+             (x-constants-regexp (regexp-opt x-constants 'words))
+             (x-functions-regexp (regexp-opt x-functions 'words)))
+
+        `(
+          (,x-types-regexp . font-lock-type-face)
+          (,x-constants-regexp . font-lock-constant-face)
+          (,x-functions-regexp . font-lock-function-name-face)
+          (,x-keywords-regexp . font-lock-keyword-face)
+          )))
+
+;;;###autoload
+(define-derived-mode xp-mode fundamental-mode "xp mode"
+  "Major mode for editing XP (Linden Scripting Language)…"
+
+  ;; code for syntax highlighting
+  (setq font-lock-defaults '((xp-font-lock-keywords)))
+  (add-to-list 'auto-mode-alist '("\\.xp\\'" . xp-mode)))
+
+(add-to-list 'auto-mode-alist '("\\.xp\\'" . xp-mode))
 
 (provide 'utils)
