47450d0833260c630a2763b6fd8d1207078a3ac9
Dmitriy Iptyshev <diptyshev@ptsecurity.com>
2019-02-11 17:48:00 +0700

    Add fast switch last buffer

--- settings/custom_pref.el
+++ settings/custom_pref.el
@@ -151,4 +151,12 @@
 
 (define-key isearch-mode-map (kbd "C-h") 'isearch-del-char) ;; Возможность удалять символы в режиме isearch
 
+(defun switch-to-previous-buffer ()
+  "Switch to previously open buffer.
+Repeated invocations toggle between the two most recently open buffers."
+  (interactive)
+  (switch-to-buffer (other-buffer (current-buffer) 1)))
+
+(global-set-key (kbd "C-c b") #'switch-to-previous-buffer)
+
 (provide 'custom_pref)
