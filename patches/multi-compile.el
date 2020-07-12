6a89d945b99a108b029be2948a9c320f468c53ad
Dmitriy Iptyshev <diptyshev@ptsecurity.com>
2020-07-13 03:08:27 +0700

    Delete old files

--- patches/multi-compile.el
+++
@@ -1,9 +0,0 @@
-(defun multi-compile-run ()
-  "Choice target and start compile."
-  (interactive)
-  (let* ((template (multi-compile--get-command-template))
-         (command (or (car-safe template) template))
-         (default-directory (if (listp template) (eval-expression (cadr template)) default-directory)))
-	(progn (setq compile-command command)
-		   (compilation-start
-			(multi-compile--fill-template command)))))
